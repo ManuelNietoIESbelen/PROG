f9538fb3de04ded509dbf21dbc8fa1d5af89a2ea
ManuelNietoIESbelen <mniejim1703@g.educaand.es>
2024-10-21 14:43:33 +0200

    cambios

--- eclipse-workspace/r01ejercicios/src/r01ejercicios/r01e23.java
+++
@@ -1,9 +0,0 @@
-package r01ejercicios;
-
-import java.util.*;
-
-public class r01e23 {
-  public static void main(String[] args) {
-    System.out.println("Suponiendo que la formula de la pontecia es I=");
-  }
-}

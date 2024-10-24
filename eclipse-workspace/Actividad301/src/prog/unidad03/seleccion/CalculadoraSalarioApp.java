5ae3d731afc2c267f1467eadfa40ebd8729614f2
ManuelNietoIESbelen <mniejim1703@g.educaand.es>
2024-10-24 08:33:22 +0200

    ej

--- eclipse-workspace/Actividad301/src/prog/unidad03/seleccion/CalculadoraSalarioApp.java
+++ eclipse-workspace/Actividad301/src/prog/unidad03/seleccion/CalculadoraSalarioApp.java
@@ -9,20 +9,21 @@ public class CalculadoraSalarioApp {
   private static final int EUROS_POR_HORAS_NORMALES = 16;
 
   public static void main(String[] args) {
-    Scanner sc= new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
     System.out.println("Introduce el numero de horas trabajadas");
-    int horas=Integer.parseInt(sc.nextLine());
-    int horasExtra= horas-HORAS_NORMALES_MAXIMAS;
-    int horasNormales= horas-horasExtra;
-    double salario= horasNormales*EUROS_POR_HORAS_NORMALES+horasExtra*EUROS_POR_HORAS_EXTRAS;
-    
-    if (horasExtra>1) {
-          System.out.printf("El salario por trabajar %d horas normales y %d horas extra es %f €",horasNormales,horasExtra,salario);
-    }else {
-      System.out.printf("El salario por trabajar %d horas normales es de: %f €",horas,salario);
+    int horas = Integer.parseInt(sc.nextLine());
+
+    int horasExtra = horas - HORAS_NORMALES_MAXIMAS;
+    int horasNormales = horas - horasExtra;
+    double salario = horasNormales * EUROS_POR_HORAS_NORMALES + horasExtra * EUROS_POR_HORAS_EXTRAS;
+
+    if (horasExtra > 1) {
+      System.out.printf("El salario por trabajar %d horas normales y %d horas extra es %f €", horasNormales, horasExtra,
+          salario);
+    } else {
+      System.out.printf("El salario por trabajar %d horas normales es de: %f €", horas, salario);
     }
-    
-    
+
   }
 
 }

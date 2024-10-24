c2129bc19248a3815c7291efe15283014068aafb
ManuelNietoIESbelen <mniejim1703@g.educaand.es>
2024-10-24 08:32:23 +0200

    ejs

--- eclipse-workspace/Actividad301/src/prog/unidad03/seleccion/CalculadoraSalarioApp.java
+++ eclipse-workspace/Actividad301/src/prog/unidad03/seleccion/CalculadoraSalarioApp.java
@@ -4,13 +4,17 @@ import java.util.Scanner;
 
 public class CalculadoraSalarioApp {
 
+  private static final int HORAS_NORMALES_MAXIMAS = 40;
+  private static final int EUROS_POR_HORAS_EXTRAS = 20;
+  private static final int EUROS_POR_HORAS_NORMALES = 16;
+
   public static void main(String[] args) {
     Scanner sc= new Scanner(System.in);
     System.out.println("Introduce el numero de horas trabajadas");
     int horas=Integer.parseInt(sc.nextLine());
-    int horasExtra= horas-40;
+    int horasExtra= horas-HORAS_NORMALES_MAXIMAS;
     int horasNormales= horas-horasExtra;
-    double salario= horasNormales*16+horasExtra*20;
+    double salario= horasNormales*EUROS_POR_HORAS_NORMALES+horasExtra*EUROS_POR_HORAS_EXTRAS;
     
     if (horasExtra>1) {
           System.out.printf("El salario por trabajar %d horas normales y %d horas extra es %f €",horasNormales,horasExtra,salario);
